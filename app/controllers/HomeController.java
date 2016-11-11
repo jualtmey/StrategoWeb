5930bc7307f6ab31672543bfb421767069af735c
redscarwolf <kieser.ma@gmail.com>
2016-11-11 14:26:53 +0100

    toJson, jquery ajay add

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,11 +1,18 @@
 package controllers;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
 import de.htwg.stratego.StrategoApp;
 import de.htwg.stratego.aview.tui.TextUI;
 import de.htwg.stratego.controller.IStrategoController;
 import de.htwg.stratego.model.ICell;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
@@ -16,25 +23,32 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result strategoWui(String command) {
-    	IStrategoController controller = StrategoApp.getInstance().getIStrategoController();
-        return ok(strategoWui.render(controller));
-    }
-    
+    private IStrategoController controller;
+
     public Result strategoTui(String command) {
     	TextUI tui = StrategoApp.getInstance().getTui();
     	tui.processInputLine(command);
     	return ok(tui.printTUI());
     }
-    
+
     public Result strategoIndex() {
         return ok(strategoIndex.render());
+    }
+
+    public Result strategoWui() {
+        controller = StrategoApp.getInstance().getIStrategoController();
+        return ok(strategoWui.render(controller));
+    }
+
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result add() {
+        JsonNode json = request().body().asJson();
+        controller.add(json.findPath("column").intValue() ,json.findPath("row").intValue(), json.findPath("rank").intValue());
+        return refresh();
+    }
+
+    public Result refresh() {
+        return ok(controller.toJson());
     }
 
 }
--- public/javascripts/strategoWui.js
+++ public/javascripts/strategoWui.js
@@ -19,3 +19,50 @@ function removeSelectedClass() {
 }
 
 Array.from(passable).forEach(e => e.addEventListener('mouseout', removeSelectedClass));
+
+
+$(function() {
+    $(".selectCell").click(function(event){
+        $.ajax({
+            type: "POST",
+            url: '/strategoWui/add',
+            contentType: "text/json",
+            data: JSON.stringify(
+                {
+                    'row': 1,
+                    'column': 1,
+                    'rank': 5,
+                }),
+            success: function(responseTxt) {
+                let strategoJson = JSON.parse(responseTxt);
+                refreshField(strategoJson.field, strategoJson.playerOne, strategoJson.playerTwo);
+            },
+            error:  function() {
+                alert("Error");
+            },
+        });
+
+    });
+});
+
+function refreshField(field, playerOne, playerTwo) {
+    // $(".cellBorder[data-row='0'][data-column='3'] .figure").attr('src', 'assets/images/figures/0.svg');
+    field.innerField.forEach(item => {
+        let cell = $(".cellBorder[data-row='" + item.row + "'][data-column='" + item.column+ "'] .figure")
+        if (item.containsCharacter) {
+            if(item.character.player === playerOne) {
+                cell.addClass("player1");
+                cell.removeClass("player2");
+            }
+            if(item.character.player === playerTwo) {
+                cell.addClass("player2");
+                cell.removeClass("player1");
+            }
+            cell.attr('src', 'assets/images/figures/'+ item.character.rank +'.svg');
+        } else {
+            cell.removeClass("player1");
+            cell.removeClass("player2");
+            cell.attr('src', 'assets/images/figures/empty.svg');
+        }
+    });
+}

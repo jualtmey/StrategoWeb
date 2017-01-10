e8a237242b0720c0c4274b048eb58afb0adfb6fc
redscarwolf <kieser.ma@gmail.com>
2017-01-10 16:39:33 +0100

    lobby1: add profileList

--- app/controllers/WebSocketController.java
+++ app/controllers/WebSocketController.java
@@ -8,7 +8,11 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import de.htwg.stratego.StrategoModule;
 import de.htwg.stratego.controller.IMultiDeviceStrategoController;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.play.PlayWebContext;
 import org.pac4j.play.java.Secure;
+import org.pac4j.play.store.PlaySessionStore;
 import play.mvc.Controller;
 import play.mvc.LegacyWebSocket;
 import play.mvc.Result;
@@ -16,15 +20,20 @@ import play.mvc.WebSocket;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 public class WebSocketController extends Controller {
 
     public static ActorRef lobby;
 
-    @Inject public WebSocketController(ActorSystem actorSystem) {
+    @Inject
+    public WebSocketController(ActorSystem actorSystem) {
         lobby = actorSystem.actorOf(LobbyActor.props(actorSystem));
     }
+
+    @Inject
+    private PlaySessionStore playSessionStore;
 
     public Result strategoWui() {
         Injector injector = Guice.createInjector(new StrategoModule());
@@ -39,6 +48,12 @@ public class WebSocketController extends Controller {
     @Secure(clients = "OidcClient")
     public Result lobby() {
         System.out.println("<<<<<<<<<. - .>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<lobby wurde aufgerufen!!! TEST"); // TODO test
-        return ok(views.html.lobby.render());
+        return ok(views.html.lobby.render(getProfiles()));
+    }
+
+    private List<CommonProfile> getProfiles() {
+        final PlayWebContext context = new PlayWebContext(ctx(), playSessionStore);
+        final ProfileManager<CommonProfile> profileManager = new ProfileManager(context);
+        return profileManager.getAll(true);
     }
 }
